c0f9cc12084c76b8eadb239970d0122273d8610f
Viesturs Spūlis <v.spuulis@gmail.com>
2020-05-27 11:32:28 +0300

    Minor changes

--- Dice/app/routes.py
+++ Dice/app/routes.py
@@ -1,13 +1,13 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app import app
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route("/submit-status")
+@app.route("/submit-status", methods=["POST"])
 def submit_status():
-    return "Thanks"
+    return "Received_status"
 
 @app.route("/get-status")
 def get_status():
